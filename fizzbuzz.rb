0ff8f8ddb92894863477513ddfce984a6358f175
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 05:04:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "Fizzbuzz"
+    "Fizzbuzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif number % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else 
     nil
   end
